78b10237c163342d828711bb0722996044994410
Dmitry Demonarrr <d.ursus.d@gmail.com>
2021-12-14 12:20:21 +0300

    Added button choose file

--- mw_musicrenamerapp.cpp
+++ mw_musicrenamerapp.cpp
@@ -5,7 +5,13 @@ MW_MusicRenamerApp::MW_MusicRenamerApp(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MW_MusicRenamerApp)
 {
-    ui->setupUi(this);
+    //setup main window settings
+    //using functions for setup, set and change parts of main window
+    addToolBar(Qt::RightToolBarArea, createRightToolBar());
+    setCentralWidget(createCentralWidget());
+
+
+    ui->setupUi(this);//for the moment of writing idk what it's, so don't delete
 }
 
 MW_MusicRenamerApp::~MW_MusicRenamerApp()
@@ -13,3 +19,50 @@ MW_MusicRenamerApp::~MW_MusicRenamerApp()
     delete ui;
 }
 
+//metjhods:
+QWidget *MW_MusicRenamerApp::createCentralWidget()
+{
+    QWidget *centralWgt = new QWidget(); //this one will be returned
+
+    //here is creating left part of central widget
+    QStringListModel *strlm_fileNames = new QStringListModel(m_strl_fileNames_temp, 0);
+    QListView *lv_fileNames = new QListView();
+    lv_fileNames->setModel(strlm_fileNames);
+    //here is creating of right part of central widget
+    QStringListModel *strlm_temp = new  QStringListModel(m_strl_fileNames_temp, 0);
+    QListView *lv_renamingResult = new QListView();
+    lv_renamingResult->setModel(strlm_temp);
+
+    //layout setup
+    QHBoxLayout *hbxl_centralWgtLayout = new QHBoxLayout();
+    hbxl_centralWgtLayout->addWidget(lv_fileNames);
+    hbxl_centralWgtLayout->addWidget(lv_renamingResult);
+
+    centralWgt->setLayout(hbxl_centralWgtLayout);
+    centralWgt->resize(400, 400);
+    centralWgt->show();
+
+    return centralWgt;
+}
+
+QToolBar *MW_MusicRenamerApp::createRightToolBar()
+{
+    QToolBar *rightToolBar = new QToolBar("Right Tool Bar"); //this one will be returned
+    //creats and connect button for pick file
+    QPushButton *pbtn_chooseFile = new QPushButton("Choose file...");
+    connect(pbtn_chooseFile,
+                SIGNAL(clicked()),
+                SLOT(slotChooseFile())
+            );
+    rightToolBar->addWidget(pbtn_chooseFile);
+
+    ;return rightToolBar;
+}
+
+//slots:
+
+//giving list of files for the left part of central widget
+void MW_MusicRenamerApp::slotChooseFile()
+{
+   m_strl_fileNames_temp = QFileDialog::getOpenFileNames(0, "Choose file for rename", "");
+}
--- mw_musicrenamerapp.h
+++ mw_musicrenamerapp.h
@@ -3,6 +3,17 @@
 
 #include <QMainWindow>
 #include <QtWidgets>
+#include <QAction>
+#include <QFileDialog>
+#include <QMessageBox>
+
+//to do:
+//add Qstr will contain var from slotChooseFile //done
+//   func createCentralWidget
+//add func that will work with Qstr
+//add central Wgt, with 2 parts. Left part contain list of names before renaming,
+//right part contain result. Maybe ist possible to to come up with new ideas
+//Something wrong with central widget. It's doesn't display
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MW_MusicRenamerApp; }
@@ -12,8 +23,12 @@ class MW_MusicRenamerApp : public QMainWindow
 {
     Q_OBJECT
 private:
-    void createRightToolBar();
-    void createCentralWidget();
+    //var's and const's
+    QStringList m_strl_fileNames_temp; //feels like bad decision, its possible to do way much better
+
+    //functions
+    QToolBar *createRightToolBar();
+    QWidget *createCentralWidget();
 
 
 
